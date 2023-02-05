1148eb74d86802c597b560c31f2907f1a2d62b25
mikhita <mikheilispiriani@yahoo.com>
2023-02-05 17:52:19 +0400

    3.3: Phonebook backend step3

--- index.js
+++ index.js
@@ -40,6 +40,18 @@ app.get("/info", (req, res) => {
   );
 });
 
+app.get("/api/persons/:id", (req, res) => {
+  const id = Number(req.params.id);
+  console.log(id);
+  const person = persons.find((person) => person.id === id);
+  console.log(person);
+  if (person) {
+    res.json(person);
+  } else {
+    res.status(404).end();
+  }
+});
+
 const PORT = 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
