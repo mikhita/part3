b5df386f58465142a45a10c5ea2c0a0c2593a04f
mikhita <mikheilispiriani@yahoo.com>
2023-02-06 19:00:26 +0400

    I am trying to connects to notes

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ app.use(
   })
 );
 app.use(cors());
-app.use(express.static('build'))
+app.use(express.static("build"));
 
 // morgan((tokens, req, res) => {
 //     let logData = `${tokens.method(req, res)} ${tokens.url(
@@ -35,26 +35,26 @@ app.use(express.static('build'))
 
 //     return logData;
 //   })
-let persons = [
+let notes = [
   {
     id: 1,
-    name: "Arto Hellas",
-    number: "040-123456",
+    content: "HTML is easy",
+    important: true,
   },
   {
     id: 2,
-    name: "Ada Lovelace",
-    number: "39-44-5323523",
+    content: "Browser can execute only JavaScript",
+    important: false,
   },
   {
     id: 3,
-    name: "Dan Abramov",
-    number: "12-43-234345",
+    content: "GET and POST are the most important methods of HTTP protocol",
+    important: true,
   },
   {
+    content: "alscm'",
+    important: true,
     id: 4,
-    name: "Mary Poppendieck",
-    number: "39-23-6423122",
   },
 ];
 
@@ -62,67 +62,67 @@ app.get("/", (req, res) => {
   res.send("<h1>Hello World!</h1>");
 });
 
-app.get("/api/persons", (req, res) => {
-  res.json(persons);
+app.get("/api/notes", (req, res) => {
+  res.json(notes);
 });
-app.get("/info", (req, res) => {
-  const date = new Date();
-  res.send(
-    `<p>PhoneBook has info for ${persons.length} people.</p><p> Response date: ${date}</p>`
-  );
-});
+// app.get("/info", (req, res) => {
+//   const date = new Date();
+//   res.send(
+//     `<p>PhoneBook has info for ${persons.length} people.</p><p> Response date: ${date}</p>`
+//   );
+// });
 
-app.get("/api/persons/:id", (req, res) => {
-  const id = Number(req.params.id);
-  console.log(id);
-  const person = persons.find((person) => person.id === id);
-  console.log(person);
-  if (person) {
-    res.json(person);
-  } else {
-    res.status(404).end();
-  }
-});
+// app.get("/api/persons/:id", (req, res) => {
+//   const id = Number(req.params.id);
+//   console.log(id);
+//   const person = persons.find((person) => person.id === id);
+//   console.log(person);
+//   if (person) {
+//     res.json(person);
+//   } else {
+//     res.status(404).end();
+//   }
+// });
 
-app.delete("/api/persons/:id", (req, res) => {
-  const id = Number(req.params.id);
-  persons.filter((person) => person.id !== id);
-  res.status(204).end();
-});
+// app.delete("/api/persons/:id", (req, res) => {
+//   const id = Number(req.params.id);
+//   persons.filter((person) => person.id !== id);
+//   res.status(204).end();
+// });
 
-const generateId = () => {
-  const maxId = persons.length > 0 ? Math.floor(Math.random() * 1000000000) : 0;
-  return maxId + 1;
-};
+// const generateId = () => {
+//   const maxId = persons.length > 0 ? Math.floor(Math.random() * 1000000000) : 0;
+//   return maxId + 1;
+// };
 
-app.post("/api/persons", (req, res) => {
-  const body = req.body;
+// app.post("/api/persons", (req, res) => {
+//   const body = req.body;
 
-  if (!body.name || !body.number) {
-    return res.status(400).json({
-      error: "content missing",
-    });
-  }
+//   if (!body.name || !body.number) {
+//     return res.status(400).json({
+//       error: "content missing",
+//     });
+//   }
 
-  const name = body.name;
-  const hasExactName = (name) => (person) => person.name === name;
+//   const name = body.name;
+//   const hasExactName = (name) => (person) => person.name === name;
 
-  if (persons.some(hasExactName(name))) {
-    return res.status(400).json({
-      error: "name must be unique",
-    });
-  }
+//   if (persons.some(hasExactName(name))) {
+//     return res.status(400).json({
+//       error: "name must be unique",
+//     });
+//   }
 
-  const person = {
-    id: generateId(),
-    name: body.name,
-    number: body.number,
-  };
+//   const person = {
+//     id: generateId(),
+//     name: body.name,
+//     number: body.number,
+//   };
 
-  persons = persons.concat(person);
+//   persons = persons.concat(person);
 
-  res.json(person);
-});
+//   res.json(person);
+// });
 
 const PORT = process.env.PORT || 3001;
 console.log(PORT);
